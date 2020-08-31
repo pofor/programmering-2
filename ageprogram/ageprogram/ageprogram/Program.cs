9e65306f0a6b1ffeb5b26433337f7b9147eb5d53
pofor <pontus.forslermott@elev.ga.ntig.se>
2020-08-31 13:38:40 +0200

    Age problem commit

---
+++ ageprogram/ageprogram/ageprogram/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ageprogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt namn");
+
+            string namn = Console.ReadLine();
+
+            Console.WriteLine("Ditt namn är: " + namn);
+
+            Console.WriteLine("Enter your age:");
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Your age is: " + age);
+
+            Console.WriteLine("är du vid liv");
+
+            Console.WriteLine("Lever du: y/n");
+            var answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                Console.WriteLine("Bra att du lever");
+            }
+            else
+            {
+                Console.WriteLine("Good luck next life pleb!");
+            }
+
+
+        }
+    }
+}
