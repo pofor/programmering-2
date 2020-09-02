4775b66f2a83f3626b2e115af2f8598197af7c44
pofor <pontus.forslermott@elev.ga.ntig.se>
2020-09-02 10:31:45 +0200

    Loops commit

--- ageprogram/ageprogram/ageprogram/Program.cs
+++ ageprogram/ageprogram/ageprogram/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks.Dataflow;
 
 namespace ageprogram
 {
@@ -6,32 +7,53 @@ namespace ageprogram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv ditt namn");
+            while (true) {
+                Console.WriteLine("Skriv ditt namn");
 
-            string namn = Console.ReadLine();
+                string namn = Console.ReadLine();
 
-            Console.WriteLine("Ditt namn är: " + namn);
+                Console.WriteLine("Ditt namn är: " + namn);
 
-            Console.WriteLine("Enter your age:");
+                Console.WriteLine("Enter your age:");
 
-            int age = Convert.ToInt32(Console.ReadLine());
+                int age = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Your age is: " + age);
+                Console.WriteLine("Your age is: " + age);
 
-            Console.WriteLine("är du vid liv");
+                Console.WriteLine("är du vid liv");
 
-            Console.WriteLine("Lever du: y/n");
-            var answer = Console.ReadLine();
-            if (answer == "y")
-            {
-                Console.WriteLine("Bra att du lever");
+                Console.WriteLine("Lever du: y/n");
+                var answer = Console.ReadLine();
+                if (answer == "y")
+                {
+                    Console.WriteLine("Bra att du lever");
+                }
+                else
+                {
+                    Console.WriteLine("Good luck next life pleb!");
+                }
+                Console.WriteLine("du har dessa sekunder kvar att leva! P.S du dör på 0");
+                for (int i = 5; i > -1; i--)
+                {
+                    Console.WriteLine(i);
+                }
+                Console.WriteLine("Du är du död välj vilket öde du får! Skriv himlen eller helvete för att välja");
+                var hell = Console.ReadLine();
+                switch (hell)
+                {                    
+                    case "helvete":
+                        Console.WriteLine("du kom till himlen");
+                        break;
+                    case "himlen":
+                        Console.WriteLine("du kom till helvetet.");
+                        break;
+                    default:
+                        Console.WriteLine("Skit i det då...");
+                        break;
+                }
+                break;
             }
-            else
-            {
-                Console.WriteLine("Good luck next life pleb!");
-            }
-
-
         }
+
     }
 }
