2061f4e9e01d97d9db24cb61c4e27b263ce2b0bf
pofor <54797719+pofor@users.noreply.github.com>
2020-09-03 10:58:16 +0200

    array

---
+++ array/hemdatorarray/hemdatorarray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace hemdatorarray
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new string[5];
+            
+            for(int i = 4; i >= 0; i--)
+            {
+                names[i] = Console.ReadLine();
+            }
+            
+            foreach (var name in names)
+            {
+                Console.Write(name + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
