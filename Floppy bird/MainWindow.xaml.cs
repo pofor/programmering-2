d365600d1be3dfb0ea87ae1c1015b1f61244e5c8
pofor <54797719+pofor@users.noreply.github.com>
2021-04-29 09:55:24 +0200

    Jag har lagt till min flappybird
    Flappybirden är tillagd och jag har gjort så att den kan röra på sig upp och ner.  Jag fixade så att programmet kunde starta.

--- Floppy bird/MainWindow.xaml.cs
+++ Floppy bird/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Threading;
+
 namespace Floppy_bird
 {
     /// <summary>
@@ -20,9 +22,93 @@ namespace Floppy_bird
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        DispatcherTimer gameTimer = new DispatcherTimer();
+        double score;
+        int gravity = 8;
+        bool gameOver;
+        Rect flappyBirdHitBox;
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            gameTimer.Tick += MainEventTimer;
+            gameTimer.Interval = TimeSpan.FromMilliseconds(20);
+            StartGame();
+
+        }
+
+        private void MainEventTimer(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                flappyBird.RenderTransform = new RotateTransform(-20, flappyBird.Width / 2, flappyBird.Height / 2);
+                gravity = -8;
+            }
+            if (e.Key == Key.R && gameOver == true)
+            {
+                StartGame();
+            }
+
+
+        }
+
+        private void KeyIsUP(object sender, KeyEventArgs e)
+        {
+            flappyBird.RenderTransform = new RotateTransform(5, flappyBird.Width / 2, flappyBird.Height / 2);
+            gravity = 8;
+        }
+
+
+        private void StartGame()
+        {
+            MyCanvas.Focus();
+
+            int temp = 300;
+
+            score = 0;
+
+            gameOver = false;
+            Canvas.SetTop(flappybird, 190);
+
+            foreach (var x in MyCanvas.Children.OfType<Image>())
+            {
+                if ((string)x.Tag == "obs1")
+                {
+                    Canvas.SetLeft(x, 500);
+                }
+                if ((string)x.Tag == "obs2")
+                {
+                    Canvas.SetLeft(x, 800);
+                }
+                if ((string)x.Tag == "obs3")
+                {
+                    Canvas.SetLeft(x, 1100);
+                }
+                if ((string)x.Tag == "cloud")
+                {
+                    Canvas.SetLeft(x, 300 + temp );
+                    temp = 800;
+                }
+
+            }
+
+            gameTimer.Start();
+
+
+        }
+
+        private void EndGame()
+        {
+
         }
     }
 }
